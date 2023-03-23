9d043d86e637351090bca0998645b06a79c832d8
Sebastian Castro <sncastro20@gmail.com>
2023-03-23 01:30:42 -0300

    first commit

---
+++ gerTec-system/ClientApp/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+// const windmill = require('@windmill/react-ui/config')
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        bottom: '0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)',
+      }}
+  }
+      ,
+  plugins: [],
+}
---
+++ gerTec-system/Entity/EstudentEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace gerTec_system.Entity
+{
+    public class EstudentEntity
+    {
+        private long? id { get; set; }
+        private string name { get; set; }      
+            private string phone { get; set; } = string.Empty;
+        private string phoneNumber { get; set; }
+        = string.Empty;
+
+    }
+}
